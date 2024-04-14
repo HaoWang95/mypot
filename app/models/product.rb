9c812ec933a443c7499180aabbb6c55f2ad03de0
HaoWang95 <alanwang0028@gmail.com>
2024-04-14 15:07:53 +1000

    update: product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+
+  # all model attributes should present
+  validates :title, :description, :image_url, presence: true
+
+  validates :title, uniqueness: { message: "product title should be unique" }, length: { minimum: 1, maximum: 150 }
+
+  # The price should be a valid number and larger than 0
+  validates :price, numericality: { greater_than: 1, message: "must be greater than $0" }
+  validates :price, numericality: { less_than_or_equal_to: 200, message: "must be smaller or equal to $200"}
+
+  validates :image_url, allow_blank: true, format: {
+                          with: %r{\.(gif|jpg|png)\z}i,
+                          message: "must be a url for GIF, JPG or PNG",
+                        }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,27 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "product attributes should not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price should be higher than 0" do
+    mock_product = Product.new(title: "test", description: "test description", image_url: "lorem.jpg", price: -1)
+    assert mock_product.invalid?
+    assert mock_product.errors[:price].any?
+    assert_equal mock_product.errors[:price], ["must be greater than $0"]
+  end
+
+  test "product price should be smaller than $200" do
+    mock_product = Product.new(title: "test", description: "test description", image_url: "lorem.jpg", price: 300.0)
+    assert mock_product.invalid?
+    # mock_product.errors.map { |error| puts error.message }
+    assert_equal mock_product.errors[:price], ["must be smaller or equal to $200"]
+  end
 end
