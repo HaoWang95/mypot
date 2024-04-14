ea938cf7b02196094236c353abbba2ead12d1fc2
HaoWang95 <alanwang0028@gmail.com>
2024-04-15 01:09:12 +1000

    update: store index cache

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    # @page_title = "Rails book store"
   end
 end
--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    # use assert_select to pick nav item
+    assert_select 'nav a', minimum: 4
   end
 end
